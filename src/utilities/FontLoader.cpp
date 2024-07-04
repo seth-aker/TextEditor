a5daa6ab648f8cbd3a9637a6e7a9a925fc2604b2
saker132 <saker132@gmail.com>
2024-07-04 18:56:20 -0400

    Adding font loader class helpers

--- src/utilities/FontLoader.cpp
+++ src/utilities/FontLoader.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <string>
 #include <filesystem>
+#include <iostream>
 
 FontLoader::FontLoader() {};
 
@@ -20,5 +21,24 @@ bool FontLoader::loadFonts(std::filesystem::path& dirPath) {
 }
 
 FontType FontLoader::detectType(const std::string_view fontName) const {
+    std::size_t typePos = fontName.find_last_of("-");
+    if (typePos == std::string::npos) {
+        return FontType::REGULAR;
+    }
+    std::string_view typeString = fontName.substr(typePos + 1);
+    if (typeString == "Regular.ttf") {
+        return FontType::REGULAR;
+    }
+    else if (typeString == "Bold.ttf") {
+        return FontType::BOLD;
+    }
+    else if (typeString == "Italic.ttf") {
+        return FontType::ITALIC;
+    }
+    else if (typeString == "BoldItalic.ttf") {
+        return FontType::BOLDITALIC;
+    }
+
+    throw "Error: Font Not Recognized";
 
 }
